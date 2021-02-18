40eb4c4880795ec52c61bfd511138374469048a3
Jesus Sierra <jnsierrac@gmail.com>
2021-02-18 14:27:10 -0500

    Ajuste al nombre del pipeline para backend

--- server-acceso-datos/execute-jar.sh
+++ server-acceso-datos/execute-jar.sh
@@ -1,2 +1,3 @@
-sh wait-for-postgres.sh service-postgres12
+sh wait-for-postgres.sh $SERVER_BD
+sh wait-for-discovery.sh $SERVER_DISCOVERY
 java -jar -Dspring.profiles.active=$PROFILE_JAR api-acceso-datos.jar 
