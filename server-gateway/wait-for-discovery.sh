0b85a13e548e1de81e6ffd61bff4768db940f59d
Jesus Sierra <jnsierrac@gmail.com>
2021-02-18 12:02:58 -0500

    Ajuste

---
+++ server-gateway/wait-for-discovery.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#wait-for-discovery.sh
+
+host="$1"
+status_code="500"
+
+while [ "$status_code" != "200" ]; do
+
+    url=http://"$host":1111/actuator
+    echo Url a testear  "$url"
+    status_code=$(timeout 2s curl --write-out %{http_code} --silent --output /opt/salida "$url" )
+
+    if [ "$status_code" != "200" ]; then
+       echo "Servicio de descubrimiento no disponible" > /opt/validacion
+    fi
+done
+
+echo "Servicio de descubrimiento disponible"
